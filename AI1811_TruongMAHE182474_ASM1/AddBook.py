05d91bbce4995e3b1f000cbaabeb4d1630fc0478
Mai Anh Trường <139731971+MaiAnhTruong@users.noreply.github.com>
2024-02-18 22:18:12 +0700

    Add files via upload

---
+++ AI1811_TruongMAHE182474_ASM1/AddBook.py
@@ -0,0 +1,8 @@
+def add_book(library):
+    bid = input("Enter book id: ")
+    title = input("Enter book title: ")
+    author = input("Enter book author: ")
+    node = Node(bid, title, author)
+    library.add_node(node)
+    print(f"Book {title} added successfully.")
+    
